45a34bf07df99a96b9898c1b07af084d5f012c74
KEKS1C <105987084+KEKS1C@users.noreply.github.com>
2023-04-30 14:25:48 +0300

    Ещё немного фиксов

--- common/defines/rp_defines.lua
+++ common/defines/rp_defines.lua
@@ -1,2 +1,6 @@
 NDefines.NGame.START_DATE = "1993.1.1.1"
 NDefines.NGame.END_DATE = "2000.1.1.1"
+NDefines.NCountry.MIN_STABILITY = -1.0
+NDefines.NCountry.MIN_WAR_SUPPORT = -1.0
+NDefines.NBuildings.BASE_FACTORY_REPAIR = 0.2
+NDefines.NBuildings.BASE_FACTORY_REPAIR_FACTOR = 0.95
